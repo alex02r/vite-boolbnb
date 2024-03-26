00643222fe01cab962f13945f6188164eac16770
alex02r <alex.giochi02@gmail.com>
2024-03-26 17:07:14 +0100

    upgrade: funzione che esegue la chiamata API

--- src/pages/AppSearched.vue
+++ src/pages/AppSearched.vue
@@ -8,7 +8,19 @@ export default {
         }
     },
     methods: {
-        
+        //funzione che esegue la ricerca degli partments
+        searchApartments(){
+            let value = ''/* this.$router.params.value */
+            //eseguiamo la chimata API 
+            axios.get(`${store.baseUrl}/search/${value}`).then(response => {
+                //controlliamo se ha restituito qualcosa
+                if (response.data.apartments) {
+                    console.log(response.data.apartments);
+                }else{
+                    console.log('non è stato trovato nulla');
+                }
+            })
+        }
     },
 }
 </script>

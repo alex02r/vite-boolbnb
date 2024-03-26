19843ca7af685c91c99d15e7b3cb537095d49709
alex02r <alex.giochi02@gmail.com>
2024-03-26 18:25:13 +0100

    upgrade: gestione della paginazione

--- src/pages/AppSearched.vue
+++ src/pages/AppSearched.vue
@@ -4,7 +4,10 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            store
+            store,
+            apartments: [],
+            currentPage: 1,
+            lastPage: null
         }
     },
     created(){
@@ -12,12 +15,16 @@ export default {
     },
     methods: {
         //funzione che esegue la ricerca degli partments
-        searchApartments(){
+        searchApartments(page_num){
             //eseguiamo la chimata API 
-            axios.get(`${store.baseUrl}/api/apartments/${this.$route.params.address}`).then(response => {
+            axios.get(`${store.baseUrl}/api/apartments/${this.$route.params.address}`, { params: { page: page_num } }).then(response => {
                 //controlliamo se ha restituito qualcosa
                 if (response.data.success) {
                     console.log(response.data.apartments);
+                    //recupero i risultati
+                    this.apartments = response.data.apartments.data
+                    this.currentPage = response.data.apartments.current_page;
+                    this.lastPage = response.data.apartments.last_page;
                 }else{
                     console.log('non è stato trovato nulla');
                 }

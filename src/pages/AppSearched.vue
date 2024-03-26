cde83d01ad19ebb197fc89efc9c62e5b01ed4a3f
alex02r <alex.giochi02@gmail.com>
2024-03-26 19:37:28 +0100

    upgrade: aggiunto v-for per visualizzare gli elementi cercati, aggiunta paginazione

--- src/pages/AppSearched.vue
+++ src/pages/AppSearched.vue
@@ -34,8 +34,18 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-
+    <div v-for="(apartment, index) in apartments" :key="index">
+        <div v-if="apartment.show">
+            <h5>{{ apartment.title }}</h5>
+        </div>
+    </div>
+    <div class="row"> 
+        <div class="col-12">
+            <ul class="pagination d-flex justify-content-center">
+                <li> <button :class="currentPage == 1 ? 'disabled' : ''" class="btn btn-square btn-outline-success me-3 fw-bold" @click="searchApartments(currentPage - 1)">Precedente</button> </li>
+                <li> <button :class="currentPage == lastPage ? 'disabled' : ''" class="btn btn-outline-success fw-bold" @click="searchApartments(currentPage + 1)">Successivo</button> </li>
+            </ul>
+        </div>
     </div>
 </template>
 <style lang="">

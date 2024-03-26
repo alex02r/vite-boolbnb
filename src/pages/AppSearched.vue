265f344811c4f8349ae67b11ed124a59dc007c35
alex02r <alex.giochi02@gmail.com>
2024-03-26 16:58:44 +0100

    upgrade: importazione del file store

--- src/pages/AppSearched.vue
+++ src/pages/AppSearched.vue
@@ -1,7 +1,15 @@
 <script>
+import store from '../store.js';
 import axios from 'axios';
 export default {
-    
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        
+    },
 }
 </script>
 <template lang="">

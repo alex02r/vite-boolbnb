35ac90d5d91b6dae153d84d4ffe1e6118f2a1d58
DorinVieru <dorino.vieru@gmail.com>
2024-03-26 17:03:08 +0100

    up: modifica della home page, importazione bootstrap, modifica app ed eliminazione style di default

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -10,24 +10,20 @@ export default {
     <div>
         <div class="container-fluid jumbo">
             <div class="row justify-content-center">
-                <div class="col-4 home-title text-center py-3">
-                    <h1 class="fw-bold">Boolfolio</h1>
-                    <h4 class="fst-italic">Il tuo gestionale preferito made Boolean!</h4>
-                </div>
             </div>
         </div>
         <hr>
         <div class="container my-5"> 
             <div class="row"> 
                 <div class="col-6 mt-5 align-self-center mb-4">
-                    <h3>Scopi i nostri progetti</h3>
+                    <h3>Appartamenti</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda adipisci quibusdam aliquam, asperiores maiores, nobis, harum ad nesciunt perferendis beatae exercitationem molestias odio et quos nam fugit corporis voluptatum esse.</p>
                 </div>
                 <div class="col-6 mb-4">
-                    <a href="http://localhost:5174/blog" class="text-decoration-none"><img src="../../public/projects.png" alt="logo" width="700px"></a>
+                    <a href="#" class="text-decoration-none"><img src="../../public/airbnb-img.jpg" alt="logo" width="600px"></a>
                 </div>
                 <div class="col-6 float-start mb-5">
-                    <a href="http://localhost:5174/contacts" class="text-decoration-none"><img src="../../public/contacts.png" alt="logo" width="700px"></a>
+                    <a href="#" class="text-decoration-none"><img src="../../public/airbnb-uomo.jpg" alt="logo" width="600px"></a>
                 </div>
                 <div class="col-6 align-self-center ps-5 mb-5">
                     <h3>Contattaci</h3>
@@ -40,11 +36,10 @@ export default {
 
 <!-- SEZIONE STYLE -->
 <style lang="scss" scoped>
-@use '../styles/partials/variables' as *;
 @use '../styles/generals.scss' as *;
 
 .jumbo {
-    background-image: url('../../public/website-jumbo.png');
+    background-image: url('../../public/airbnb-home.png');
     background-size: cover;
     background-position: center;
     height: 700px;
@@ -58,13 +53,13 @@ export default {
     }
 }
 
-h1 {
-    color: #6C63FF;
-}
-
 hr {
-    color: #00c1d4;
+    color: #dd0b04;
     border: 5px solid;
     width: 100%;
+}
+
+img{
+    border-radius: 10px;
 }
 </style>

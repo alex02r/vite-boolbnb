156c55e49390f0acc1442b24ea2af5de0858f501
alex02r <alex.giochi02@gmail.com>
2024-03-27 10:31:48 +0100

    upgrade: funzione che se presente restituisce l'immagine dal db altrimenti ne visualizza una di default

--- src/components/AppApartment.vue
+++ src/components/AppApartment.vue
@@ -9,12 +9,23 @@ export default {
             store        
         }
     },
-
+    methods: {
+        //funzione che restituisce l'url dell'immagine
+        getImg(img){
+            let path =`${store.baseUrl}/img/image.png` //immagine di default
+            //controlle se l'immagine è presente
+            if (img != null) {
+                path = `${store.baseUrl}/storage/${img}`
+            }
+            //restituisco il path
+            return path 
+        }
+    },
 }
 </script>
 <template lang="">
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-        <img src="" alt="" class="app-img">
+        <img :src="getImg(data.cover_img)" alt="" class="app-img">
         <!-- badge per la sponsorizzazione -->
         <span class="badge rounded-pill text-bg-light">Sponsorizzato</span>
         <div class="app-body">

b926a1117c6075d4068f8dca8dfde6c2db99479e
alex02r <alex.giochi02@gmail.com>
2024-03-27 10:22:03 +0100

    upgrade: import store, aggiunta visualizzazione titolo e indirizzo di apartment

--- src/components/AppApartment.vue
+++ src/components/AppApartment.vue
@@ -1,20 +1,27 @@
 <script>
+import { store } from '../store';
 export default {
     props:{
         data: Object
-    }
+    },
+    data() {
+        return {
+            store        
+        }
+    },
+
 }
 </script>
 <template lang="">
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-lg-2">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
         <img src="" alt="" class="app-img">
         <!-- badge per la sponsorizzazione -->
         <span class="badge rounded-pill text-bg-light">Sponsorizzato</span>
         <div class="app-body">
-            <h5 class="">Nome apartments</h5>
-            <h5 class="text-secondary">Indirizzo dell' appartamento</h5>
+            <h6 class="">{{ data.title }}</h6>
+            <h6 class="text-secondary">{{ data.address }}</h6>
             <div class="info">
-                <i class="fas fa-bed">2</i>
+                <i class="fas fa-bed"></i> 2
             </div>
         </div>
     </div>

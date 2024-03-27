088916941477d3cbb6015a0dd53e4c77f906feb9
DorinVieru <dorino.vieru@gmail.com>
2024-03-27 14:11:28 +0100

    up: modifica della home, sistemazione immagine e search

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -15,24 +15,26 @@ export default {
 <template lang="">
     <div class="container-fluid jumbo">
         <div class="row justify-content-center">
+            <h1 class="logo fw-bold pt-3 home-title">BoolBnB</h1>
         </div>
     </div>
-    <hr>
     <div class="container my-5"> 
-        <div class="row"> 
-            <Searchbar />
-            <div class="col-6 mt-5 align-self-center mb-4">
-                <h3>Appartamenti</h3>
+        <div class="row">
+            <div class="col-12 mb-5">
+                <p class="search"><Searchbar /></p>
+            </div>
+            <div class="col-md-6 col-12 mt-5 align-self-center mb-4">
+                <a href="#" class="text-decoration-none"><h3>Appartamenti</h3></a> 
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda adipisci quibusdam aliquam, asperiores maiores, nobis, harum ad nesciunt perferendis beatae exercitationem molestias odio et quos nam fugit corporis voluptatum esse.</p>
             </div>
-            <div class="col-6 mb-4">
+            <div class="col-md-6 col-12 mb-4">
                 <a href="#" class="text-decoration-none"><img src="../../public/airbnb-img.jpg" alt="logo" width="600px"></a>
             </div>
-            <div class="col-6 float-start mb-5">
+            <div class="col-md-6 col-12 float-md-start mb-5">
                 <a href="#" class="text-decoration-none"><img src="../../public/airbnb-uomo.jpg" alt="logo" width="600px"></a>
             </div>
-            <div class="col-6 align-self-center ps-5 mb-5">
-                <h3>Contattaci</h3>
+            <div class="col-md-6 col-12 align-self-center ps-5 mb-5">
+                <a href="#" class="text-decoration-none"><h3>Contattaci</h3></a>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et enim debitis natus recusandae quasi beatae aliquid totam consectetur ipsa consequatur, quos id, dolorem suscipit alias corrupti molestias sunt accusamus laboriosam!</p>
             </div>
         </div>
@@ -43,24 +45,29 @@ export default {
 <style lang="scss" scoped>
 
 .jumbo {
-    background-image: url('../../public/airbnb-home.png');
+    background-image: url('../../public/img-jumbo.jpg');
     background-size: cover;
     background-position: center;
-    height: 700px;
+    height: 390px;
+    position: relative;
+    text-align: center;
 
     .home-title {
+        font-size: 65px;
+        color: white;
         position: absolute;
-        top: 230px;
-        left: 640px;
-        border: 5px solid #6C63FF;
-        border-radius: 20px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 }
 
-hr {
-    color: #dd0b04;
-    border: 5px solid;
-    width: 100%;
+h3{
+    color: #F15B5D;;
+}
+
+.search {
+    padding-left: 200px;
 }
 
 img{

d39baf67b368355ecf23c9edb4eb43cbe0360efc
alex02r <alex.giochi02@gmail.com>
2024-03-27 10:10:25 +0100

    upgrade: struttura per la visualizzazione dell'apartments

--- src/components/AppApartment.vue
+++ src/components/AppApartment.vue
@@ -4,10 +4,19 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-lg-2">
+        <img src="" alt="" class="app-img">
+        <!-- badge per la sponsorizzazione -->
+        <span class="badge rounded-pill text-bg-light">Sponsorizzato</span>
+        <div class="app-body">
+            <h5 class="">Nome apartments</h5>
+            <h5 class="text-secondary">Indirizzo dell' appartamento</h5>
+            <div class="info">
+                <i class="fas fa-bed">2</i>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="">
+<style lang="scss" scoped>
     
 </style>

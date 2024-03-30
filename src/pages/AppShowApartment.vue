26872308536c8109c2ebf6b2158d1207794e158c
alex02r <alex.giochi02@gmail.com>
2024-03-30 11:09:18 +0100

    upgrade: chiamta API che restituisce l'aparment cercato

--- src/pages/AppShowApartment.vue
+++ src/pages/AppShowApartment.vue
@@ -4,20 +4,31 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            store
+            store,
+            apartment: []
         }
     },
     created() {
-        
+        this.getApartment();
     },
     methods: {
-        
+        getApartment(){
+            axios.get(`${store.baseUrl}/api/single/apartment/${this.$route.params.slug}/${this.$route.params.id}`).then(response => {
+                if (response.data.success) {
+                    console.log(response.data);
+                    this.apartment = response.data.apartment;
+                }else{
+                    console.log(response.data.error);
+                    this.$router.push({ name: 'not-found'})
+                }
+            })
+        }
     },
 }
 </script>
 <template lang="">
     <div>
-        
+        {{ apartment }}
     </div>
 </template>
 <style lang="">

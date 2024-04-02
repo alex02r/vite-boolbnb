89fdd6445f820012f379c54a042b93eb872f8690
DorinVieru <dorino.vieru@gmail.com>
2024-04-02 15:28:44 +0200

    up: create map

--- src/components/AppMap.vue
+++ src/components/AppMap.vue
@@ -2,33 +2,56 @@
 <script>
 import { store } from '../store.js';
 import axios from 'axios';
+import tt from '@tomtom-international/web-sdk-maps';
 
 export default {
     name: 'AppMap',
+    props: {
+        lat: String,
+        lon: String,
+    },
 
     data() {
         return {
             store,
         }
     },
+    mounted() {
+        this.setupMap();
+    },
     methods: {
-       
+        setupMap() {
+            let lon = this.lon;
+            let lat = this.lat;
+            let center = [lon, lat];
+
+            const map = tt.map({
+                key: '9CMceOdItBPcoD8aScoApx2EnxYIj4u9',
+                container: 'map',
+                center: center,
+                zoom: 12,
+                radius: 20000
+            });
+
+            const marker = new tt.Marker().setLngLat(center).addTo(map);
+            
+        }
     },
 }
 </script>
 
 <!-- SEZIONE HTML -->
 <template lang="">
-  <div class="container">
-    <div class="row">
         <div class="col-12">
-            
+            <div id='map' class="map-container rounded-3"></div>
         </div>
-    </div>
-  </div>
 </template>
 
 <!-- SEZIONE STYLE -->
 <style lang="scss" scoped>
+
+.map-container {
+    height: 400px;
+}
 
 </style>

10cc7d2b85f1ce919e5e5c911529cdab34a74d6e
DorinVieru <dorino.vieru@gmail.com>
2024-04-02 12:45:13 +0200

    up: creazione componente AppMap per visualizzare la mappa

---
+++ src/components/AppMap.vue
@@ -0,0 +1,34 @@
+<!-- SEZIONE JS SCRIPT -->
+<script>
+import { store } from '../store.js';
+import axios from 'axios';
+
+export default {
+    name: 'AppMap',
+
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+       
+    },
+}
+</script>
+
+<!-- SEZIONE HTML -->
+<template lang="">
+  <div class="container">
+    <div class="row">
+        <div class="col-12">
+            
+        </div>
+    </div>
+  </div>
+</template>
+
+<!-- SEZIONE STYLE -->
+<style lang="scss" scoped>
+
+</style>

76626a2afbc7c2d7a73a1c77239bcef2860b969d
DorinVieru <dorino.vieru@gmail.com>
2024-04-09 14:50:54 +0200

    fix: aggiustata la grandezza della modale nella search

--- src/pages/AppSearched.vue
+++ src/pages/AppSearched.vue
@@ -79,7 +79,7 @@ export default {
 <template lang="">
     <div class="container my-4">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6">
+            <div class="col-lg-6">
                 <form @submit.prevent="searchApartments()">
                     <div class="input-group mb-1">
                         <button type="button" class="btn btn-light border" @click="showModal = !showModal"> <i class="fas fa-sliders"></i> filtri</button>
@@ -117,12 +117,12 @@ export default {
                             <hr>
                             <!-- body filtri -->
                             <div class="row">
-                                <div class="col-12 col-md-6">
+                                <div class="col-lg-6">
                                     <h3>Filtra il raggio dei km</h3>
                                     <label for="distance" class="form-label">Raggio: {{ distance }}km</label>
                                     <input type="range" class="form-range" min="0" max="40" step="1" id="distance" name="distance" v-model="distance">
                                 </div>
-                                <div class="col-12 col-md-6">
+                                <div class="col-lg-6">
                                 </div>
                                 <hr>
                                 <div class="col-12">
@@ -238,6 +238,10 @@ export default {
         left: 50%;
         transform: translateX(-50%);
         z-index: 999;
+
+        @media only screen and (max-width: 991px) {
+            width: 95vw;
+        }
     }
     .input-group-text.text_width{
         width: 20px;
